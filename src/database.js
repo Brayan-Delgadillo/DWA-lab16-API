fbb2106ab8c680df71676c98eab494879d341232
Brayan-Delgadillo <brayan.delgadillo.a@tecsup.edu.pe>
2023-12-05 12:26:20 -0500

    API para peliculas, socios y prestamos

---
+++ src/database.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://master:321@cluster0.lqxe26r.mongodb.net/sistemaAPI',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(db => console.log('Database is connected'));
+
+
+const peliculaSchema = new mongoose.Schema({
+    titulo: String,
+    genero: String,
+    director: String,
+    actores: [String]
+  });
+
+const socioSchema = new mongoose.Schema({
+    nombre: String,
+    apellido: String,
+});
+  
+const prestamoSchema = new mongoose.Schema({
+    fechaPrestamo: Date,
+    fechaDevolucion: Date,
+    pelicula: String,
+    socio: Number,
+    numeroCinta: Number
+});
+
+const Pelicula = mongoose.model('Pelicula', peliculaSchema);
+const Socio = mongoose.model('Socio', socioSchema);
+const Prestamo = mongoose.model('Prestamo', prestamoSchema);
+
+  module.exports = {
+    mongoose,
+    Pelicula,
+    Socio,
+    Prestamo
+  };
---
+++ src/index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const { mongoose, Pelicula, Socio, Prestamo } = require('./database');
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/peliculas', async (req, res) => {
+    try {
+        const peliculas = await Pelicula.find({});
+    
+        res.json(peliculas);
+      } catch (error) {
+        res.status(500).json({ message: 'Error al obtener las películas', error: error.message });
+      }
+});
+
+app.post('/peliculas', async (req, res) => {
+    try {
+        const nuevaPelicula = new Pelicula(req.body);
+    
+        const peliculaGuardada = await nuevaPelicula.save();
+    
+        res.status(201).json(peliculaGuardada);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al agregar la película', error: error.message });
+    }
+});
+  
+app.get('/prestamos', async (req, res) => {
+    try {
+        const prestamos = await Prestamo.find({});
+    
+        res.json(prestamos);
+        } catch (error) {
+        res.status(500).json({ message: 'Error al obtener los prestamos', error: error.message });
+        }
+});
+
+app.post('/prestamos', async (req, res) => {
+    try {
+        const nuevoPrestamo = new Prestamo(req.body);
+    
+        const prestamoGuardado = await nuevoPrestamo.save();
+    
+        res.status(201).json(prestamoGuardado);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al agregar el prestamo', error: error.message });
+    }
+});
+
+
+app.get('/socios', async (req, res) => {
+    try {
+        const socios = await Socio.find({});
+    
+        res.json(socios);
+        } catch (error) {
+        res.status(500).json({ message: 'Error al obtener los socios', error: error.message });
+        }
+});
+
+app.post('/socios', async (req, res) => {
+    try {
+        const nuevoSocio = new Socio(req.body);
+    
+        const socioGuardado = await nuevoSocio.save();
+    
+        res.status(201).json(socioGuardado);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al agregar el socio', error: error.message });
+    }
+});
+
+app.listen(PORT)
+console.log(`Server on port ${PORT}`)
+
